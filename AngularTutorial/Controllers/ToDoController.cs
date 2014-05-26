97e9aeae80a644a06f1ab0ef07a40e485da53639
WilyMo021 <wfayette@indiana.edu>
2014-05-26 15:36:48 -0400

    Initial Save

---
+++ AngularTutorial/Controllers/ToDoController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using AngularTutorial.Models;
+
+namespace AngularTutorial.Controllers
+{
+    public class ToDoController : ApiController
+    {
+        private ToDoContext db = new ToDoContext();
+
+        // GET api/ToDo
+        public IEnumerable<Todo> GetTodos(string q = null, string sort = null, bool desc = false, int? limit = null, int offset = 0)
+        {
+            var list = ((IObjectContextAdapter)db).ObjectContext.CreateObjectSet<Todo>();
+
+            IQueryable<Todo> items = string.IsNullOrEmpty(sort)
+                ? list.OrderBy(o => o.Priority)
+                : list.OrderBy(String.Format("it.{0} {1}", sort, desc ? "DESC" : "ASC"));
+
+            if (!string.IsNullOrEmpty(q) && q != "undefined")
+            {
+                items = items.Where(t => t.Text.Contains(q));
+            }
+
+            if (offset > 0)
+            {
+                items = items.Skip(offset);
+            }
+
+            if (limit.HasValue)
+            {
+                items = items.Take(limit.Value);
+            }
+
+            return items;
+        }
+
+        // GET api/ToDo/5
+        public Todo GetTodo(int id)
+        {
+            Todo todomodel = db.Todoes.Find(id);
+            if (todomodel == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return todomodel;
+        }
+
+        // PUT api/ToDo/5
+        public HttpResponseMessage PutTodo(int id, Todo todomodel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != todomodel.Id)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(todomodel).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/ToDo
+        public HttpResponseMessage PostTodo(Todo todomodel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Todoes.Add(todomodel);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, todomodel);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = todomodel.Id }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/ToDo/5
+        public HttpResponseMessage DeleteTodo(int id)
+        {
+            Todo todomodel = db.Todoes.Find(id);
+            if (todomodel == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Todoes.Remove(todomodel);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, todomodel);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AngularTutorial/Scripts/app.js
@@ -0,0 +1,92 @@
+﻿var TodoApp = angular.module("TodoApp", ["ngResource", "ngRoute"]).config(function ($routeProvider) {
+    $routeProvider.
+        when('/', { controller: ListCtrl, templateUrl: 'list.html' }).
+        when('/new', { controller: CreateCtrl, templateUrl: 'details.html' }).
+        when('/edit/:editId', {controller: EditCtrl, templateUrl: 'details.html'}).
+        otherwise({ redirectTo: '/' });
+});
+
+TodoApp.factory('Todo', function ($resource) {
+    return $resource('api/Todo/:id', { id: '@id' }, { update: { method: 'PUT' } });
+});
+
+var CreateCtrl = function ($scope, $location, Todo) {
+    $scope.action = "Add";
+    $scope.save = function () {
+        Todo.save($scope.item, function () {
+            $location.path('/');
+        });
+    }
+};
+
+var EditCtrl = function ($scope, $location, $routeParams, Todo) {
+    var Id = $routeParams.editId;
+    $scope.item = Todo.get({ id: Id });
+    $scope.action = "Edit";
+
+    $scope.save = function () {
+        Todo.update({ id: Id }, $scope.item, function () {
+            $location.path('/');
+        });
+    }
+}
+
+var ListCtrl = function ($scope, $location, Todo) {
+    $scope.search = function () {
+        Todo.query({
+            q: $scope.query,
+            sort: $scope.sort_order,
+            desc: $scope.is_descending,
+            limit: $scope.limit,
+            offset: $scope.offset
+        }, function (data) {
+            $scope.todos = $scope.todos.concat(data);
+            if (data.length < 20) {
+                $scope.has_more = false;
+            }
+        });
+    }
+
+
+
+    $scope.show_more = function () {
+        $scope.offset += $scope.limit;
+        $scope.search();
+    }
+
+    $scope.sort = function (column) {
+        if (column == $scope.sort_order) {
+            $scope.is_descending = !$scope.is_descending;
+        } else {
+            $scope.sort_order = column;
+            $scope.is_descending = false;
+        }
+        $scope.reset();
+    };
+
+    $scope.reset = function () {
+        $scope.limit = 20;
+        $scope.offset = 0;
+        $scope.todos = [];
+        $scope.has_more = true;
+        $scope.search();
+    }
+
+    $scope.delete = function () {
+        var id = this.todo.Id;
+        Todo.delete({id: id}, function() {
+            $('#todo_' + id).fadeOut();
+        });
+    }
+
+    $scope.edit = function () {
+
+    }
+
+    $scope.sort_order = "Priority";
+    $scope.is_descending = false;
+
+    $scope.reset();
+
+    
+};
